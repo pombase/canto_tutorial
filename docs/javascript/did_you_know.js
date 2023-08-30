6c3e88dac1890b561d06950b128b98f2bfc84bad
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-31 00:49:58 +0200

    fix canto tutorial link

--- docs/javascript/did_you_know.js
+++ docs/javascript/did_you_know.js
@@ -1,7 +1,7 @@
 // A simple script that displays a random did you know message at the footer
 
 const allMessages = [
-    'Canto has ways to save time during curation, visit our <a href="/productivity">productivity page</a>.',
+    'Canto has ways to save time during curation, visit our <a href="https://pombase.github.io/canto_tutorial/productivity">productivity page</a>.',
     'You can submit high-throughput datasets of genetic and physical interactions to <a href="https://wiki.thebiogrid.org/doku.php/contribute">BioGrid</a>, and they will appear in PomBase.',
     'You can submit high-throughput annotations of GO, phenotype, modification, or gene expression data, visit our <a href="https://www.pombase.org/submit-data">data submission documentation</a>.',
     'You can submit sequence-linked data, and they will appear in PomBase genome browser. Visit our <a href="https://www.pombase.org/submit-data">data submission documentation</a>.'
